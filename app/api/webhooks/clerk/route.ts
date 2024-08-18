a775b88d29ad6449fb518a3b354c987353b1f5ef
Aminul Islam Alvi <aminulialvi@gmail.com>
2024-08-18 21:37:02 +0600

    Update import

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,5 @@
 /* eslint-disable camelcase */
-import { clerkClient } from '@clerk/nextjs';
-import { WebhookEvent } from '@clerk/nextjs/server';
+import { WebhookEvent, clerkClient } from '@clerk/nextjs/server';
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { Webhook } from 'svix';
