7575ccf8a8d31c1429b63ff8b8f1bacc7c159dea
Aminul Islam Alvi <aminulialvi@gmail.com>
2024-08-24 04:43:58 +0600

    Fix typo

--- components/landing/pricing.tsx
+++ components/landing/pricing.tsx
@@ -79,7 +79,7 @@ export const PricingSection = () => {
       </h2>
 
       <h2 className='text-3xl md:text-4xl text-center font-bold mb-4'>
-        Get unlimitted access
+        Get unlimited access
       </h2>
 
       <h3 className='md:w-1/2 mx-auto text-xl text-center text-muted-foreground pb-14'>
