0b136b6f65dc3822b46b1210f7c0afc7739fbd04
Aminul Islam Alvi <aminulialvi@gmail.com>
2024-08-24 04:45:13 +0600

    Refactor code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,27 +7,13 @@ import NavBar from '@/components/landing/navbar';
 import { PricingSection } from '@/components/landing/pricing';
 import { ServicesSection } from '@/components/landing/services';
 import { TestimonialSection } from '@/components/landing/testimonial';
-import { Collection } from '@/components/shared/Collection';
 import { getAllImages } from '@/lib/actions/image.action';
-import Image from 'next/image';
-import Link from 'next/link';
 
 const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
-
-  const images = await getAllImages({ page, searchQuery });
-
   return (
     <>
-      {/* <section className='sm:mt-12'>
-        <Collection
-          hasSearch={true}
-          images={images?.data}
-          totalPages={images?.totalPage}
-          page={page}
-        />
-      </section> */}
       <NavBar />
       <HeroSection />
       <BenefitsSection />
